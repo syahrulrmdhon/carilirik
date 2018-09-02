129fadc5d6491fe5e850240dd8d895d90c529954
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-02 16:51:09 +0700

    call api with axios

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/layouts/Navbar";
 import Index from "./components/layouts/Index";
+import { Provider } from "./context";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar/>
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Index}/>
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar/>
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Index}/>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
